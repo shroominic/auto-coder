3f582c04344b6cf45c7846443b3d378e4c2ad52e
Shroominic <db2002dominic@gmail.com>
2023-03-15 02:16:19 +0100

    cli structure

---
+++ src/__main__.py
@@ -0,0 +1,17 @@
+import argparse
+
+from src.models import Issue
+    
+
+# Set up argument parser
+parser = argparse.ArgumentParser(description='Process a GitHub issue link')
+parser.add_argument('issue', help='Link to the GitHub issue')
+
+# Parse the command line arguments
+args = parser.parse_args()
+
+issue: Issue = args.issue
+
+print(f'Processing GitHub issue: {issue}')
+
+issue.solve()
---
+++ src/models/issue.py
@@ -0,0 +1,23 @@
+
+
+class Issue:
+    """ 
+    Represents an issue in a repository
+    Issues are seen as TODOs in the codebase
+    They need to get solved by the bot
+    """
+    
+    def __init__(self, repo, issue_number):
+        self.repo = repo
+        self.issue_number = issue_number
+        
+        self.title = None
+        self.description = None
+        self.comments = []
+        self.tags = []
+        
+    def solve(self):
+        """ 
+        Solves the issue by creating a pull request
+        """
+        pass
