de1ef4d05ce5eef1d85b65a745548d7bd80a6eed
Shroominic <db2002dominic@gmail.com>
2023-04-07 13:04:05 +0200

    format

--- autocoder/celery/tasks/issue.py
+++ autocoder/celery/tasks/issue.py
@@ -6,6 +6,7 @@ def solve_issue(issue_id: int) -> bool:
     """ Solve issue queued by user """
     pass
 
+
 @celery.task(name="solve_revision")
 def solve_revision(issue_id: int) -> bool:
     """ Solve revision queued by user """
