63240580f03f51c2eed177321fefd3094bccd2a3
Shroominic <db2002dominic@gmail.com>
2023-04-10 22:38:53 +0200

    fix issue endpoint

--- autocoder/api/endpoints/issue.py
+++ autocoder/api/endpoints/issue.py
@@ -1,19 +1,32 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter
+from pydantic import BaseModel
 from fastapi.responses import JSONResponse
 from autocoder.database.models import Issue
 
 router = APIRouter()
 
+class IssueCreate(BaseModel):
+    """ Create an issue """
+    issue_url: str
+    access_token: str | None
+
 
 @router.post("/api/issue/create")
-def create_issue(request: Request):
+async def create_issue(issue: IssueCreate):
     """ Create an issue in the database """
-    Issue.from_url(
-        url          = request.json().get("issue_url"), 
-        access_token = request.json().get("access_token")
-    )
-    
-    return JSONResponse(
-        status_code=200, 
-        content={"message": "Issue created"}
-    )
+    try:
+        Issue.from_url(
+            issue_url = issue.issue_url, 
+            access_token = issue.access_token or None
+            )
+        return JSONResponse(
+            status_code=200, 
+            content={"status": "ok"}
+            )
+    except Exception as e:
+        return JSONResponse(
+            status_code=400, 
+            content={
+                "status": "error", 
+                "message": str(e)
+            })
