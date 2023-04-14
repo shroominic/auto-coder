31253f7280b9d06bd17d5731cab33e8fe1ea37cc
Shroominic <db2002dominic@gmail.com>
2023-04-14 12:03:18 +0200

    .

--- autocoder/api/routes.py
+++ autocoder/api/routes.py
@@ -1,10 +1,12 @@
 from fastapi.staticfiles import StaticFiles
-from autocoder.api.endpoints import home, issue, healthcheck
-from autocoder.api.app import app
+from fastapi import APIRouter
+from .core import app
+from .endpoints import home, healthcheck, issue, login
 
 
 app.mount("/static", StaticFiles(directory="autocoder/api/static"), name="static")
 
 app.include_router(home.router)
 app.include_router(healthcheck.router)
-app.include_router(issue.router)
+app.include_router(issue.router, prefix="/api")
+app.include_router(login.router, prefix="/api")
