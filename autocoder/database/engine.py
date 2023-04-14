81975e6e771782599f28f428d14216448b0d9a8c
Shroominic <db2002dominic@gmail.com>
2023-04-14 12:04:22 +0200

    .

--- autocoder/database/engine.py
+++ autocoder/database/engine.py
@@ -1,13 +1,15 @@
 from os import getenv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-from .base import Base
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 
-DATABASE_URL = getenv("DATABASE_URL") or "sqlite:///autocoder/database/db.sqlite3"
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-session = scoped_session(SessionLocal)
+DATABASE_URL = getenv("DATABASE_URL") or "sqlite+aiosqlite:///autocoder/database/db.sqlite3"
 
-def init_db():
-    Base.metadata.create_all(bind=engine)
+
+engine = create_async_engine(DATABASE_URL)
+session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with session_maker() as session:
+        yield session
