ffce5245180818fb4524bdb32ab1f7126dee9c0f
Shroominic <db2002dominic@gmail.com>
2023-04-14 12:04:18 +0200

    .

--- autocoder/database/base.py
+++ autocoder/database/base.py
@@ -1,31 +1,5 @@
+from sqlalchemy import inspect
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 
-
 Base = declarative_base()
-
-class SpecialBase(Base):
-    __abstract__ = True
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-    @classmethod
-    def get(cls, session, **kwargs):
-        return session.query(cls).filter_by(**kwargs).first()
-
-    @classmethod
-    def create(cls, session, **kwargs):
-        instance = cls(**kwargs)
-        session.add(instance)
-        session.commit()
-        return instance
-
-    @classmethod
-    def get_or_create(cls, session, **kwargs):
-        # check if table exists
-        if not cls.__table__.exists(bind=session.bind):
-            cls.__table__.create(bind=session.bind)
-        instance = cls.get(session, **kwargs)
-        if instance:
-            return instance
-        return cls.create(session, **kwargs)
