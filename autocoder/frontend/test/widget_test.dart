9b6fabbea9c88a2036e802bcfcb0834d1b9d2a56
Shroominic <db2002dominic@gmail.com>
2023-04-28 14:52:08 +0200

    fix basic test

--- autocoder/frontend/test/widget_test.dart
+++ autocoder/frontend/test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:autocodr/main.dart';
+import 'package:autocodr/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const AutoCodr());
+    await tester.pumpWidget(AutoCodr());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
