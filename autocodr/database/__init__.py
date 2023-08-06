6af0155b5379137abac6aff2e7a7419d2ea0c75c
Shroominic <db2002dominic@gmail.com>
2023-08-07 01:22:43 +0200

    new database

---
+++ autocodr/database/__init__.py
@@ -0,0 +1,3 @@
+from autocodr.database.engine import engine, get_session
+
+__all__ = ["engine", "get_session"]
---
+++ autocodr/database/engine.py
@@ -0,0 +1,14 @@
+from sqlalchemy.ext.asyncio import create_async_engine 
+from sqlmodel.ext.asyncio.session import AsyncSession
+from autocodr import settings
+
+
+engine = create_async_engine(
+    settings.DATABASE_URL, 
+    echo=settings.DEBUG
+)
+
+
+async def get_session():
+    async with AsyncSession(engine) as session:
+        yield session
