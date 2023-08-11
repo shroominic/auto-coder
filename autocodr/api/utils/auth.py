bfcbfbb720fe7191fb4580bf2fa553547204e9d0
Shroominic <db2002dominic@gmail.com>
2023-08-11 14:51:03 +0200

    fix typing

--- autocodr/api/utils/auth.py
+++ autocodr/api/utils/auth.py
@@ -1,10 +1,12 @@
 from fastapi import Depends, HTTPException
 from fastapi_jwt_auth import AuthJWT, exceptions  # type: ignore
+
 from autocodr.database.models import User
+
 from ..core import settings
 
 
-@AuthJWT.load_config
+@AuthJWT.load_config  # type: ignore
 def get_config():
     return settings
 
@@ -13,7 +15,7 @@ async def authenticate_user(authorize: AuthJWT = Depends()):
     try:
         authorize.jwt_required()
         decoded_token = authorize.get_raw_jwt()
-        user = await User.from_email(decoded_token["sub"])
+        user = await User.from_email(decoded_token["sub"])  # type: ignore
         return user
     except exceptions.AuthJWTException as e:
         raise HTTPException(status_code=401, detail=str(e))
--- autocodr/api/utils/email.py
+++ autocodr/api/utils/email.py
@@ -1,23 +1,22 @@
 from os import getenv
-from dotenv import load_dotenv
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
-from autocoder.database.models import User
 
+from autocodr.database import models
+from dotenv import load_dotenv
+from sendgrid import SendGridAPIClient  # type: ignore
+from sendgrid.helpers.mail import Mail  # type: ignore
 
 load_dotenv()
 email_client = SendGridAPIClient(getenv("SENDGRID_API_KEY"))
 
 
 # TODO: put this into jinja2 template
-async def send_login_email(user: User, temp_token: str):
+async def send_login_email(user: models.User, temp_token: str):
     email_client.send(
         Mail(
             subject="Your Personal Login Link",
             from_email="auth@shroominic.com",
             to_emails=[user.email],
-            html_content=
-            "Click the following link to log in:<br><br>"
+            html_content="Click the following link to log in:<br><br>"
             f" - http://127.0.0.1:8000/login?token={temp_token} - <br><br>"
             "Thanks for using AutoCodr!",
         )
