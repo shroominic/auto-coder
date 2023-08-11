cc7fca2ad0134f3a58739e6aa0d7a83eb4b7d37f
Shroominic <db2002dominic@gmail.com>
2023-08-11 16:57:26 +0200

    update codebase

--- autocodr/repobox/codebase.py
+++ autocodr/repobox/codebase.py
@@ -1,49 +1,66 @@
 from codeboxapi import CodeBox  # type: ignore
 from github.Repository import Repository
 
-class CodeBase(CodeBox):
-    def __init__(self):
-        super().__init__()
-        self.codebox = CodeBox()
-        
-    @classmethod
-    async def from_repo(cls, repository: Repository, access_token=None):
-        codebase = cls()
-        await codebase._clone(repository, access_token)
-        return codebase
 
-    async def _clone(self, repository: Repository, access_token=None):
-        await self.codebox.arun(
-            f"import os; os.system('git clone {repository.clone_url} .')"
+class CodeBase(CodeBox):
+    def __init__(self, repository: Repository, access_token=None):
+        super().__init__()
+        self.repository = repository
+        self.access_token = access_token
+
+    async def astart(self, repository: Repository, access_token=None):
+        await super().astart()
+        await self.arun(
+            "import os; os.mkdir('.codebase'); os.chdir('.codebase');"   
         )
-        
+        # setup git credentials for pushing and forking
+        await self.bash(
+            f"git clone {repository.clone_url} .",
+            f"git config --global user.email 'github@autocodr.com'",
+            f"git config --global user.name 'AutoCodr'",
+            f"git config --global credential.helper store"
+        )
+        if access_token:
+            await self.bash(
+                f"git config --global credential.helper '!f() {{ echo username=git; echo password={access_token}; }}; f'"
+            )
+        await self.bash(
+            "git config --global push.default simple"
+        )
+    
+    async def bash(self, *commands: str) -> str:
+        return (
+            await self.arun(
+                "os.system('" + "; ".join(commands) + "')"
+            )
+        ).content
     
     async def read_file(self, relative_path: str) -> bytes | None:
         return (
-            await self.codebox.adownload(relative_path)
+            await self.adownload(relative_path)
         ).content
     
     async def show_tree(self) -> str | None:
         return (
-            await self.codebox.arun("import os; os.walk('.')")
+            await self.arun("os.walk('.')")
         ).content
 
     async def show_file(self, relative_path: str) -> str | None:
         # TODO: check max string length
         return (
-            await self.codebox.arun(f"open('{relative_path}').read()")
+            await self.arun(f"open('{relative_path}').read()")
         ).content
 
     async def file_exists(self, relative_path) -> bool:
         # TODO: test this
         return (
-            await self.codebox.arun(f"import os; os.path.exists('{relative_path}')")
+            await self.arun(f"os.path.exists('{relative_path}')")
         ).content == "True"
 
     async def validate_file_paths(self, file_paths):
         # TODO test this
         return bool((
-            await self.codebox.arun(
+            await self.arun(
                 f"import re; all([re.search(r'.*\\..*', path)"
                 " is not None for path in {file_paths}])"
             )
@@ -51,12 +68,12 @@ class CodeBase(CodeBox):
 
     async def create_file(self, relative_path, content) -> bool:
         return relative_path in (
-            await self.codebox.aupload(relative_path, content)
+            await self.aupload(relative_path, content)
         ).status
 
-    async def change_file(self, relative_path: str, changes: list) -> bool:
+    async def change_file(self, relative_path: str, changes: list[list[str]]) -> bool:
         return (
-            await self.codebox.arun(
+            await self.arun(
                 f"""
                 with open('{relative_path}', 'r') as f:
                     lines = f.readlines()
@@ -73,35 +90,18 @@ class CodeBase(CodeBox):
                     f.writelines(lines)
                     
                 # format file with black
-                import os
                 os.system(f"black {relative_path}")
                 """
             )
         ).type != "error"
 
     async def delete_file(self, relative_path) -> None:
-        # os.remove(os.path.join(self.path, relative_path))
-        pass
+        await self.bash(f"rm {relative_path}")
 
     async def commit_changes(self, message) -> None:
-        # self.repository.create_branch("autocoder-solving-issue")
-        # repo = Repo(self.path)
-
-        # repo.git.add(A=True)
-        # repo.index.commit(message)
-        # origin = repo.remote(name='origin')
-        # origin.push()
-        pass
-
-    def _clone_repository(self):
-        # repo_path = self.PATH + self.repository.name
-        # if not os.path.exists(repo_path):
-        #     # Pass the access token as an environment variable for the git clone command
-        #     os.environ['GIT_ASKPASS'] = f'echo {self.repository.access_token}'
-        #     os.environ['GIT_TERMINAL_PROMPT'] = '0'
-        #     Repo.clone_from(self.repository.repo_url, repo_path)
-        # return repo_path
-        pass
+        await self.bash("git add .")
+        await self.bash(f"git commit -m '{message}'")
+        await self.bash("git push origin master")
 
     def __repr__(self) -> str:
         return f"Codebase({self.repository}"
