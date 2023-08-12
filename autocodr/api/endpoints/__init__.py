8f12bad8befc729bb5b5fefd50e98cdd8e788030
Shroominic <db2002dominic@gmail.com>
2023-08-12 15:12:41 +0200

    fix home page

--- autocodr/api/endpoints/__init__.py
+++ autocodr/api/endpoints/__init__.py
@@ -1,3 +1,2 @@
 from autocodr.api.endpoints import healthcheck, home, login
 
-__all__ = ["healthcheck", "home", "login"]
--- autocodr/api/endpoints/home.py
+++ autocodr/api/endpoints/home.py
@@ -1,11 +1,19 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from autocodr.api import settings
 
 router = APIRouter()
-templates = Jinja2Templates(directory="autocoder/api/templates")
+templates = Jinja2Templates(directory="autocodr/frontend/templates/")
 
 
 @router.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+    return templates.TemplateResponse(
+        "home.html",
+        {
+            "pay_issue_link": settings.PAY_LINK_ISSUE,
+            "static": settings.STATIC_URL,
+            "request": request
+        }
+    )
